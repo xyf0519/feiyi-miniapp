ec5da38d2cbeb762ab3b002a19052a54b09d8c63
zmy-2006 <1839772842@qq.com>
2026-08-31 19:47:09 +0800

    Initial commit

---
+++ 未命名 (1)/assets/javascript/initData.js
@@ -0,0 +1,216 @@
+const resolutionInfo = {
+    "web": [
+      {
+        "label": "网页 1920",
+        "value": "1920x1080",
+        "w": 1920,
+        "h": 1080
+      },
+      {
+        "label": "网页 1440",
+        "value": "1440x1024",
+        "w": 1440,
+        "h": 1024
+      },
+      {
+        "label": "MacBook Pro 16\"",
+        "value": "1728x1117",
+        "w": 1728,
+        "h": 1117
+      },
+      {
+        "label": "MacBook Pro 14\"",
+        "value": "1512x982",
+        "w": 1512,
+        "h": 982
+      },
+      {
+        "label": "MacBook Pro",
+        "value": "1440x900",
+        "w": 1440,
+        "h": 900
+      },
+      {
+        "label": "MacBook Air 13",
+        "value": "1280x832",
+        "w": 1280,
+        "h": 832
+      },
+      {
+        "label": "iMac",
+        "value": "2240x1260",
+        "w": 2240,
+        "h": 1260
+      },
+      {
+        "label": "华为 MateBook 16s",
+        "value": "1260x840",
+        "w": 1260,
+        "h": 840
+      },
+      {
+        "label": "华为 MateBook 14s",
+        "value": "1260x840-14",
+        "w": 1260,
+        "h": 840
+      }
+    ],
+    "app": [
+      {
+        "label": "iPhone 16 Pro Max",
+        "value": "440x956-16pm",
+        "w": 440,
+        "h": 956
+      },
+      {
+        "label": "iPhone 16 Pro",
+        "value": "402x874-16p",
+        "w": 402,
+        "h": 874
+      },
+      {
+        "label": "iPhone 16 Plus",
+        "value": "430x932-16+",
+        "w": 430,
+        "h": 932
+      },
+      {
+        "label": "iPhone 16",
+        "value": "393x852-16",
+        "w": 393,
+        "h": 852
+      },
+      {
+        "label": "iPhone 15 Pro Max",
+        "value": "430x932-15pm",
+        "w": 430,
+        "h": 932
+      },
+      {
+        "label": "iPhone 15 Pro",
+        "value": "393x852-15p",
+        "w": 393,
+        "h": 852
+      },
+      {
+        "label": "iPhone 15 Plus",
+        "value": "430x932-15+",
+        "w": 430,
+        "h": 932
+      },
+      {
+        "label": "iPhone 15",
+        "value": "393x852-15",
+        "w": 393,
+        "h": 852
+      },
+      {
+        "label": "iPhone 14 Plus",
+        "value": "428x926-14+",
+        "w": 428,
+        "h": 926
+      },
+      {
+        "label": "iPhone 14",
+        "value": "390x844-14",
+        "w": 390,
+        "h": 844
+      },
+      {
+        "label": "iPhone 13 mini",
+        "value": "375x812-13m",
+        "w": 375,
+        "h": 812
+      },
+      {
+        "label": "iPhone 11",
+        "value": "414x896-11",
+        "w": 414,
+        "h": 896
+      },
+      {
+        "label": "iPhone SE",
+        "value": "320x568-se",
+        "w": 320,
+        "h": 568
+      },
+      {
+        "label": "Mate 70",
+        "value": "405x896-m70",
+        "w": 405,
+        "h": 896
+      },
+      {
+        "label": "Mate 70 Pro",
+        "value": "438x944-m70p",
+        "w": 438,
+        "h": 944
+      },
+      {
+        "label": "Mate 70 Pro+",
+        "value": "438x944-m70p+",
+        "w": 438,
+        "h": 944
+      },
+      {
+        "label": "Mate 60",
+        "value": "405x896-m60",
+        "w": 405,
+        "h": 896
+      },
+      {
+        "label": "Mate 60 Pro",
+        "value": "420x907-m60p",
+        "w": 420,
+        "h": 907
+      },
+      {
+        "label": "Mate XT-单屏",
+        "value": "336x744-mxt-s",
+        "w": 336,
+        "h": 744
+      },
+      {
+        "label": "Mate XT-双屏",
+        "value": "682x744-mxt-d",
+        "w": 682,
+        "h": 744
+      },
+      {
+        "label": "Mate XT-三屏",
+        "value": "1061x744-mxt-t",
+        "w": 1061,
+        "h": 744
+      },
+      {
+        "label": "Mate X5-展开",
+        "value": "741x832-mx5-u",
+        "w": 741,
+        "h": 832
+      },
+      {
+        "label": "Mate X5-折叠",
+        "value": "360x835-mx5-f",
+        "w": 360,
+        "h": 835
+      },
+      {
+        "label": "小米 15",
+        "value": "400x890-mi15",
+        "w": 400,
+        "h": 890
+      },
+      {
+        "label": "Samsung Galaxy S23",
+        "value": "360x7-s23",
+        "w": 360,
+        "h": 780
+      }
+    ]
+  }
+  
+const treeArr = [{"appuuid":"1992247138578333696","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-22 23:01:46","createUser":"WX20251122ZCFXR","default":false,"folderFlag":false,"id":444072,"name":"页面1","pageName":"页面1","pageScene":"web","pageType":"0","pageUuid":"1992247138616082432","remarks":"","sort":2,"status":"0","updateTime":"2025-11-22 23:01:46","updateUser":"WX20251122ZCFXR","url":"./assets/page/1992247138616082432.html","uuid":"1992247138616082432"},{"appuuid":"1992247138578333696","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-22 23:17:27","createUser":"WX20251122ZCFXR","default":false,"folderFlag":false,"id":444096,"name":"app","pageName":"app","pageScene":"app","pageType":"0","pageUuid":"1992251084038471680","sort":3,"status":"0","updateTime":"2025-11-23 03:08:02","updateUser":"WX20251122ZCFXR","url":"./assets/page/1992251084038471680.html","uuid":"1992251084038471680"},{"appuuid":"1992247138578333696","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-22 23:32:45","createUser":"WX20251122ZCFXR","default":false,"folderFlag":false,"id":444125,"name":"新手指引","pageName":"新手指引","pageScene":"app","pageType":"0","pageUuid":"1992254934002696192","sort":5,"status":"0","updateTime":"2025-11-23 03:08:12","updateUser":"WX20251122ZCFXR","url":"./assets/page/1992254934002696192.html","uuid":"1992254934002696192"},{"appuuid":"1992247138578333696","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-23 00:07:22","createUser":"WX20251122ZCFXR","default":false,"folderFlag":false,"id":444299,"name":"登录2","pageName":"登录2","pageScene":"app","pageType":"0","pageUuid":"1992263647530844160","sort":7,"status":"0","updateTime":"2025-11-23 03:08:32","updateUser":"WX20251122ZCFXR","url":"./assets/page/1992263647530844160.html","uuid":"1992263647530844160"},{"appuuid":"1992247138578333696","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-23 01:33:44","createUser":"WX20251122ZCFXR","default":false,"folderFlag":false,"id":444498,"name":"视频播放","pageName":"视频播放","pageScene":"app","pageType":"0","pageUuid":"1992285380761616384","sort":9,"status":"0","updateTime":"2025-11-23 03:08:41","updateUser":"WX20251122ZCFXR","url":"./assets/page/1992285380761616384.html","uuid":"1992285380761616384"},{"appuuid":"1992247138578333696","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-23 02:04:56","createUser":"WX20251122ZCFXR","default":false,"folderFlag":false,"id":444517,"name":"页面7","pageName":"页面7","pageScene":"app","pageType":"0","pageUuid":"1992293231345270784","sort":11,"status":"0","updateTime":"2025-11-23 03:08:56","updateUser":"WX20251122ZCFXR","url":"./assets/page/1992293231345270784.html","uuid":"1992293231345270784"},{"appuuid":"1992247138578333696","buildType":1,"codeType":"html_module","configurationInformation":"{\"freelayoutType\":\"custom\",\"minWidth\":\"\",\"maxWidth\":\"\",\"customWidth\":\"adaptive\",\"customHeight\":\"adaptive\",\"minHeight\":\"\",\"maxHeight\":\"\",\"backgroundType\":\"color\",\"background\":\"#fff\",\"grid\":\"1\",\"height\":\"\",\"width\":\"\"}","createTime":"2025-11-23 02:14:37","createUser":"WX20251122ZCFXR","default":false,"folderFlag":false,"id":444526,"name":"答题","pageName":"答题","pageScene":"app","pageType":"0","pageUuid":"1992295671863967744","sort":13,"status":"0","updateTime":"2025-11-23 03:09:08","updateUser":"WX20251122ZCFXR","url":"./assets/page/1992295671863967744.html","uuid":"1992295671863967744"}]
+
+const projectName = '未命名'
+
+const appDoc = {"1992247138616082432":"设计一个手机app，小程序形式。第一阶段：非遗传承人入驻平台 通过与6-10位非遗传承人签订合作协议并明确双方权责后，与传承人共同策划40-50集非遗系列的课程内容。 第二阶段：宣传课程内容录制与审核 传承人录制20分钟左右的课程视频，交由平台进行审核，最终发布到APP的学习频道的分类模块中。 第三阶段：用户学习与答题流程 用户可通过微信、微博、手机号等方式进行登录后即可进入新手指导，新手指导将引导用户完成首次观看与答题，从而快速地了解APP的核心玩法与流程。完成新手指导后，用户可以免费观看所有课程，系统将会自动记录用户的观看进度并支持断点续看。 用户完成单个视频教学后，自动进入答题环节。每次答题环节设置10小题，包含单选题与判断题。答对一题可获得十分，分数达到60分及以上即可过关。 第三阶段：积分规则与兑换机制   每完成一次视频观看，用户即可获得2文化币，答对一题用户即可获得5文化币，一次性全部答对所有题目可额外获得5文化币。平台将会公示《积分获取规则》，用户可在首页-个人中心-我的积分查看已有文化币数量。   进入我的积分-积分兑换，用户将积分兑换现金、文创产品。100文化币可兑换1元，兑换的现金将直接通过微信或支付宝接口提现到账。而文创兑换则支持“积分＋现金兑换”或者“积分兑换”任意一种方式，支付成功后即生成订单。\n","1992251084038471680":"增加登录页面，\n\n* 登录表单：用户名/手机号输入框、密码输入框、登录按钮\n* 辅助功能：忘记密码、注册入口、第三方登录选项\n* 底部显示平台slogan和文化底蕴提示\n"}
